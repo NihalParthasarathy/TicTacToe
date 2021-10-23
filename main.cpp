d3667fddf4cc8bba30995ab653d3f15f61935273
Bobbert Banana <hanuman.bheema@gmail.com>
2021-10-23 20:30:55 +0100

    Final

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ struct game {
 };
 
 void printBoard(game board);
-void reprint();
+bool checkWinX(game newboard);
+bool checkWinO(game newboard);
+bool checkTie(game newboard);
 
 int main() {
   struct game newboard;
@@ -21,95 +23,164 @@ int main() {
   O.turn = 1;
   X.move = 1;
   O.move = 2;
+  int Xwins = 0;
+  int Owins = 0;
+  int ties = 0;
   int BLANK = 0;
   char str[2];
+  char str2[1];
   int whosturn = X.turn;
+  bool stillPlaying = true;
+  while (stillPlaying == true) {
 
   
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      newboard.board[i][j] = BLANK;
-      cout << newboard.board[i][j] << endl;
-    }
-  }
-  printBoard(newboard);
-
-  cout << "Enter a letter followed by a number" << endl;
-
-  cin.get(str, 3);
-  cin.get();
-  cout << str << endl;
-  if (str[0] != 'a' && str[0] != 'b' && str[0] != 'c') {
-    cout << "first letter must be a b or c" << endl;
-  }
-  else if (str[1] != '1' && str[1] != '2' && str[1] != '3') {
-    cout << "second charector must be either 1 2 or 3" << endl;
-  }
-
-  else {
-    for (int row = 0; row < 1; row++) {
-      for (int colum = 0; colum < 1; colum ++) {
-
-        if (whosturn == X.turn) {
-          if (str[0] == 'a' && str[1] == '1') {
-            newboard.board[0][0] = X.move;
-          }
-          if (str[0] == 'a' && str[1] == '2') {
-            newboard.board[0][1] = X.move;
-          }
-          if (str[0] == 'a' && str[1] == '3') {
-            newboard.board[0][2] = X.move;
-          }
-          if (str[0] == 'b' && str[1] == '1') {
-            newboard.board[1][0] = X.move;
-          }
-          if (str[0] == 'b' && str[1] == '2') {
-            newboard.board[1][1] = X.move;
-          }
-          if (str[0] == 'b' && str[1] == '3') {
-            newboard.board[1][2] = X.move;
-          }
-          if (str[0] == 'c' && str[1] == '1') {
-            newboard.board[2][0] = X.move;
-          }
-          if (str[0] == 'c' && str[1] == '2') {
-            newboard.board[2][1] = X.move;
-          }
-          if (str[0] == 'c' && str[1] == '3') {
-            newboard.board[2][2] = X.move;
-          }
-        }
-	
-        if (whosturn == O.turn) {
-          if (str[0] == 'a' && str[1] == '1') {
-            newboard.board[0][0] = O.move;
-          }
-          if (str[0] == 'a' && str[1] == '2') {
-            newboard.board[0][1] = O.move;
-          }
-          if (str[0] == 'a' && str[1] == '3') {
-            newboard.board[0][2] = O.move;
-          }
-          if (str[0] == 'b' && str[1] == '1') {
-            newboard.board[1][0] = O.move;
-          }
-          if (str[0] == 'b' && str[1] == '2') {
-            newboard.board[1][1] = O.move;
-          }
-          if (str[0] == 'b' && str[1] == '3') {
-            newboard.board[1][2] = O.move;
-          }
-          if (str[0] == 'c' && str[1] == '1') {
-            newboard.board[2][0] = O.move;
-          }
-          if (str[0] == 'c' && str[1] == '2') {
-            newboard.board[2][1] = O.move;
-          }
-          if (str[0] == 'c' && str[1] == '3') {
-            newboard.board[2][2] = O.move;
-          }
-        }
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+	newboard.board[i][j] = BLANK;
       }
+    }
+    whosturn = X.turn;
+    while (checkWinX(newboard) == false && checkWinO(newboard) == false && checkTie(newboard) == false) {
+      
+      printBoard(newboard);
+      
+      cout << "Enter a letter followed by a number" << endl;
+
+      cin.get(str, 3);
+      cin.get();
+      if (str[0] != 'a' && str[0] != 'b' && str[0] != 'c') {
+	cout << "first letter must be a b or c" << endl;
+      }
+      else if (str[1] != '1' && str[1] != '2' && str[1] != '3') {
+	cout << "second charector must be either 1 2 or 3" << endl;
+      }
+
+      else {
+	for (int row = 0; row < 1; row++) {
+	  for (int colum = 0; colum < 1; colum ++) {
+
+
+	    if (whosturn == X.turn) {
+
+	  
+	      if (str[0] == 'a' && str[1] == '1' && newboard.board[0][0] == BLANK) {
+		newboard.board[0][0] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'a' && str[1] == '2' && newboard.board[1][0] == BLANK) {
+		newboard.board[1][0] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'a' && str[1] == '3' && newboard.board[2][0] == BLANK) {
+		newboard.board[2][0] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'b' && str[1] == '1' && newboard.board[0][1] == BLANK) {
+		newboard.board[0][1] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'b' && str[1] == '2' && newboard.board[1][1] == BLANK) {
+		newboard.board[1][1] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'b' && str[1] == '3' && newboard.board[2][1] == BLANK) {
+		newboard.board[2][1] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'c' && str[1] == '1' && newboard.board[0][2] == BLANK) {
+		newboard.board[0][2] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'c' && str[1] == '2' && newboard.board[1][2] == BLANK) {
+		newboard.board[1][2] = X.move;
+		whosturn = O.turn;
+	      }
+	      else if (str[0] == 'c' && str[1] == '3' && newboard.board[2][2] == BLANK) {
+		newboard.board[2][2] = X.move;
+		whosturn = O.turn;
+	      }
+	      else {
+		cout << "Iligal move! Try again" << endl;
+	      }
+	  
+	  
+	    }
+	
+	    else {
+	      if (str[0] == 'a' && str[1] == '1' && newboard.board[0][0] == BLANK) {
+		newboard.board[0][0] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'a' && str[1] == '2' && newboard.board[1][0] == BLANK) {
+		newboard.board[1][0] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'a' && str[1] == '3' && newboard.board[2][0] == BLANK) {
+		newboard.board[2][0] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'b' && str[1] == '1' && newboard.board[0][1] == BLANK) {
+		newboard.board[0][1] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'b' && str[1] == '2' && newboard.board[1][1] == BLANK) {
+		newboard.board[1][1] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'b' && str[1] == '3' && newboard.board[2][1] == BLANK) {
+		newboard.board[2][1] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'c' && str[1] == '1' && newboard.board[0][2] == BLANK) {
+		newboard.board[0][2] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'c' && str[1] == '2' && newboard.board[1][2] == BLANK) {
+		newboard.board[1][2] = O.move;
+		whosturn = X.turn;
+	      }
+	      else if (str[0] == 'c' && str[1] == '3' && newboard.board[2][2] == BLANK) {
+		newboard.board[2][2] = O.move;
+		whosturn = X.turn;
+	      }
+	      else {
+		cout << "Ilegal Move! Try again" << endl;
+	      }
+	  
+	    }
+	
+	  }
+	}
+      }
+    }
+
+    if (checkWinX(newboard) == true) {
+      cout << "X Wins!" << endl;
+      Xwins++;
+    }
+    else if(checkWinO(newboard) == true) {
+      cout << "O Wins!" << endl;
+      Owins++;
+    }
+    else if(checkTie(newboard) == true) {
+      cout << "Tie!" << endl;
+      ties++;
+    }
+    cout << "X has won " << Xwins << " times, O has won " << Owins << " times, there have been " << ties << " ties" << endl;
+    cout << "Play again?  y or n" << endl;
+    cin.get(str2, 2);
+    cin.get();
+    if (str2[0] == 'y') {
+      stillPlaying = true;
+      for (int i = 0; i < 3; i++) {
+	for (int j = 0; j < 3; j++) {
+	  newboard.board[i][j] = BLANK;
+	  cout << "reseted" << endl;
+	}
+      } 
+    }
+    else {
+      stillPlaying = false;
     }
   }
   return 0;
@@ -126,7 +197,7 @@ void printBoard(game newboard) {
     for (int j=0; j < 3; j++) {
       cout << ' ';
        if (newboard.board[i][j] == 0) {
-	 cout << '+';
+	 cout << ' ';
        }
        if (newboard.board[i][j] == 1) {
 	 cout << 'X';
@@ -138,49 +209,83 @@ void printBoard(game newboard) {
     }
     cout << '\0' << endl;
   }
-  /*for (int i = 0; i < 3; i++) {
-    cout << ' ';
-    if (newboard.board[0][i] == 0) {
-      cout << '+';
-    }
-    if (newboard.board[0][i] == 1) {
-      cout << 'X';
-    }
-    if (newboard.board[0][i] == 2) {
-      cout << 'O';
-    }
-  }
-  cout << '\0' << endl;
-  for (int i = 0; i < 3; i++) {
-    cout << ' ';
-    if (newboard.board[1][i] == 0) {
-      cout << '+';
-    }
-    if (newboard.board[1][i] == 1) {
-      cout << 'X';
-    }
-    if (newboard.board[1][i] == 2) {
-      cout << 'O';
-    }
-
-  }
-  cout << '\0' << endl;
-  for (int i = 0; i < 3; i++) {
-    cout << ' ';
-    if (newboard.board[i][2] == 0) {
-      cout << '+';
-    }
-    if (newboard.board[i][2] == 1) {
-      cout << 'X';
-    }
-    if (newboard.board[i][2] == 2) {
-      cout << 'O';
-    }
-
-  }
-  cout << '\0' << endl;
   
-  //cout << '1' << newboard.board[1][1] << ' ' << newboard.board[1][2] << ' ' << newboard.board[1][3] << endl;
-  //cout << '2' << newboard.board[2][1] << ' ' << newboard.board[2][2] << ' ' << newboard.board[2][3] << endl;
-  //acout << '3' << newboard.board[3][1] << ' ' << newboard.board[3][2] << ' ' << newboard.board[3][3] << endl;*/
+}
+
+bool checkWinX(game newboard) {
+  if (newboard.board[0][0] == 1 && newboard.board[0][1] == 1 && newboard.board[0][2] == 1) {
+    return true;
+  }
+  else if (newboard.board[0][0] == 1 && newboard.board[1][1] == 1 && newboard.board[2][2] == 1) {
+    return true;
+  }
+  else if (newboard.board[2][0] == 1 && newboard.board[1][1] == 1 && newboard.board[0][2] == 1) {
+    return true;
+  }
+  else if (newboard.board[1][0] == 1 && newboard.board[1][1] == 1 && newboard.board[1][2] == 1) {
+    return true;
+  }
+  else if (newboard.board[2][0] == 1 && newboard.board[2][1] == 1 && newboard.board[2][2] == 1) {
+    return true;
+  }
+  else if (newboard.board[0][0] == 1 && newboard.board[1][0] == 1 && newboard.board[2][0] == 1) {
+    return true;
+  }
+  else if (newboard.board[0][1] == 1 && newboard.board[1][1] == 1 && newboard.board[2][1] == 1) {
+    return true;
+  }
+  else if (newboard.board[0][2] == 1 && newboard.board[1][2] == 1 && newboard.board[2][2] == 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+bool checkWinO(game newboard) {
+  if (newboard.board[0][0] == 2 && newboard.board[0][1] == 2 && newboard.board[0][2] == 2) {
+    return true;
+  }
+  else if (newboard.board[0][0] == 2 && newboard.board[1][1] == 2 && newboard.board[2][2] == 2) {
+    return true;
+  }
+  else if (newboard.board[2][0] == 2 && newboard.board[1][1] == 2 && newboard.board[0][2] == 2) {
+    return true;
+  }
+  else if (newboard.board[1][0] == 2 && newboard.board[1][1] == 2 && newboard.board[1][2] == 2) {
+    return true;
+  }
+  else if (newboard.board[2][0] == 2 && newboard.board[2][1] == 2 && newboard.board[2][2] == 2) {
+    return true;
+  }
+  else if (newboard.board[0][0] == 2 && newboard.board[1][0] == 2 && newboard.board[2][0] == 2) {
+    return true;
+  }
+  else if (newboard.board[0][1] == 2 && newboard.board[1][1] == 2 && newboard.board[2][1] == 2) {
+    return true;
+  }
+  else if (newboard.board[0][2] == 2 && newboard.board[1][2] == 2 && newboard.board[2][2] == 2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+bool checkTie(game newboard) {
+  bool temp = true;
+  for (int row = 0; row < 3; row++) {
+    for (int colum = 0; colum < 3; colum++) {
+      if (newboard.board[row][colum] == 0) {
+	temp = false;
+	
+      }
+    }
+  }
+  if (temp == false) {
+    return false;
+  }
+  return true;
 }
